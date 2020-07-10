66da605c94d6caade9cafc5258cd1826e55e5fa1
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-10 12:16:09 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+## 1. add a Dog class
+## 2. add an instance method #bark to the Dog class
+## that will output "Woof!"
+## 3. add an instance method #sit to the Dog
+## class that will output "The Dog is sitting."
+
+
+class Dog
+
+    def bark
+        puts "Woof!"
+    end 
+
+    def sit
+        puts "The Dog is sitting"
+    end 
+
+end 
