be6e36a6956693f23a46b3f2439b507d806ecb29
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-13 20:39:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+
+
+#class Dog 
+
+
+#def bark
+  #puts "Woof!"
+  
+ # end
+#end
+  
+#def sit   
+  #puts "The Dog is sitting"
+#end 
+
+#fido = Dog.new 
+#fido.sit
+#end
+
+
+class Dog 
+    def bark 
+        puts "Woof!"
+    end
+
+    def sit 
+        puts "The Dog is sitting"
+    end
+end 
